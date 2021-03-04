e50bc8a4118771149b0670731d5651f16613c97f
wickkan <75600207+wickkan@users.noreply.github.com>
2021-03-04 21:51:45 +1100

    Update story_gen.py

--- story_gen.py
+++ story_gen.py
@@ -4,17 +4,22 @@ This is a story generator made in Python
 
 from random import choice
 
+url = 'https://github.com/wickkan'
+
 def story_gen():
     who = ['Peter Rabbit', 'Johnny Depp', 'Mickey Mouse', 'Hannah Montana']
     what = ['was eating a cake', 'flew to Paris', 'turned up to school late', 'crashed his car']
     when = ['last night', 'yesterday', 'a few days ago', 'just then']
     action = ['but unknowingly left their wallet at home.', 'but instantly regretted it.', 'and also flew to Berlin.', 'and unfortunately missed their dentist appointment.']
-    return print(choice(who) + ' ' + choice(what) + ' ' + choice(when) + ' ' + choice(action))
+    return print('\033[93m' + choice(who) + ' ' + choice(what) + ' ' + choice(when) + ' ' + choice(action) + '\033[0m')
 
 def print_story():
-    print('---------------------------')
+    print('----------------------------------------------------')
     story_gen()
-    print('---------------------------')
+    print(
+        '*****------------------------------------------*****\n''Thanks for trying: Story Generator\n''Click the link to check out more of my projects:\n',
+        url)
+    print('*****------------------------------------------*****')
 
 if __name__ == '__main__':
     print_story()
